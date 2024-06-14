eec08f215ab99af0df6a4720b18e5842755c1b90
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-13 22:33:05 -0400

    User Sergey Galich saved changes to main.py at 06/14/2024, 02:33:04 UTC time.

--- d7149d13-2178-402f-9917-9bd921d6e14b/main.py
+++ d7149d13-2178-402f-9917-9bd921d6e14b/main.py
@@ -24,11 +24,11 @@ class TradingStrategy(Strategy):
             min_price = min(last_20_prices)
             
             # Open long if current price is higher than the max of the last 20 minutes prices
-            if current_price > max_price:
+            if current_price > max_price and data['holdings']['SPY'] != 1:
                 spy_stake = 1  # 100% allocation into SPY
                 log("Opening long position in SPY")
             # Open short if current price is less than the min of the last 20 minutes prices
-            elif current_price < min_price:
+            elif current_price < min_price and data['holdings']['SPY'] != -1:
                 spy_stake = -1  # Indicates a short position; handling of short positions depends on the platform's ability to execute them
                 log("Opening short position in SPY")
         else:
